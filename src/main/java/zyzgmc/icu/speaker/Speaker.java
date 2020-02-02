63dcdd2e9d175943f9bf0cbef5475c6488f61571
SHUN.L <liushun0574@gmail.com>
2020-02-02 08:30:28 +0800

    first commit

--- src/main/java/zyzgmc/icu/speaker/Speaker.java
+++ src/main/java/zyzgmc/icu/speaker/Speaker.java
@@ -1,5 +1,6 @@
 package zyzgmc.icu.speaker;
 
+import com.google.common.reflect.TypeToken;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.spongepowered.api.Server;
@@ -18,6 +19,10 @@ import zyzgmc.icu.speaker.config.Config;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Plugin(
         id = "speaker",
@@ -58,20 +63,22 @@ public class Speaker {
         CommandSpec speaker = CommandSpec.builder()
                 .description(Text.of("显示版本信息"))
                 .permission("speaker.command.base")
-                .child(SetMessage.build(),"set","s")
+                .child(SetMessage.build(),"set")
                 .child(Help.build(),"help")
-                .child(ModeSwith.build(),"mode","m")
-                .child(SetInterval.build(),"interval","i")
-                .child(SetFixtime.build(),"fixtime","f")
                 .child(Add.build(),"add")
                 .child(ListCmd.build(), "list")
                 .child(Show.build(),"show")
                 .child(Reload.build(),"reload")
+                .child(Remove.build(),"remove", "delete")
                 .executor(new Vmessage())
                 .build();
 
         Sponge.getCommandManager().register(this, speaker,"speaker","spk");
         logger.info("插件成功加载");
-        System.out.println(Config.rootNode.getNode("All"));
+        System.out.println(Config.rootNode.getNode("All").getValue());
+
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        logger.info("当前时间是"+format.format(date));
     }
 }
---
+++ src/main/java/zyzgmc/icu/speaker/command/Remove.java
@@ -0,0 +1,48 @@
+package zyzgmc.icu.speaker.command;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.serializer.TextSerializers;
+import zyzgmc.icu.speaker.Speaker;
+import zyzgmc.icu.speaker.config.Config;
+
+import java.io.IOException;
+
+public class Remove implements CommandExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+
+        String Name = args.<String>getOne("公告名称").get();
+
+        if(Config.rootNode.getNode("All",Name).isVirtual()){
+            src.sendMessage(TextSerializers.FORMATTING_CODE.deserialize(String.format("&c%s &a不存在，请检查名称", Name)));
+        }else {
+            Config.rootNode.getNode("All").removeChild(Name);
+            src.sendMessage(TextSerializers.FORMATTING_CODE.deserialize(String.format("&c%s &a已移除", Name)));
+            try {
+                Config.save();
+                Config.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return CommandResult.success();
+    }
+    public static CommandSpec build(){
+        return CommandSpec.builder()
+                .description(Text.of("移除一个公告"))
+                .permission("speaker.command.remove")
+                .executor(new Remove())
+                .arguments(
+                        GenericArguments.onlyOne(GenericArguments.string(Text.of("公告名称")))
+                )
+                .build();
+    }
+}
